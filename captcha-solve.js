cc025ea31b68cfa8c9ab9d60279ef9a7d4154f26
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-12-11 01:41:23 +0500

    check

--- captcha-solve.js
+++ captcha-solve.js
@@ -1,26 +1,53 @@
-const puppeteerExtraStealth= require('puppeteer-extra-plugin-stealth');
-const puppeteer = require('puppeteer-extra');
-const { setTimeout } = require('timers/promises')
-
-puppeteer.use(puppeteerExtraStealth());
-
+const puppeteer = require('puppeteer');
+const { normalizeUserAgent } = require('./normalize-ua.js');
+const { Solver } = require('@2captcha/captcha-solver');
+const { readFileSync } = require('fs');
+require('dotenv').config();
+const {setTimeout } = require('timers/promises');
+const CAPTCHA_API_KEY = process.env.CAPTCHA_API_KEY;
+const solver = new Solver(CAPTCHA_API_KEY);
+console.log(`Using 2captcha API key: ${CAPTCHA_API_KEY}`);
 (async () => {
+    const userAgent = await normalizeUserAgent();
+    console.log(userAgent);
     const browser = await puppeteer.launch({
         headless: false,
+        defaultViewport: null,
         args: [
-            '--no-sandbox',
-             '--disable-setuid-sandbox',
-             '--start-maximized',
-             '--disable-infobars',
-
+            '--start-maximized',
+            `--user-agent=${userAgent}`,
+            '--no-sandbox'
         ]
     });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 });
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
-    await page.goto('https://photon-sol.tinyastro.io/');
-    await page.waitForNetworkIdle();
-    await page.screenshot({ path: 'screenshot.png' });
-    console.log('page content:', await page.content());
 
+    const [page] = await browser.pages();
+    const preloadFile = readFileSync('./inject.js', 'utf8');
+    await page.evaluateOnNewDocument(preloadFile);
+
+    page.on('console', async (msg) => {
+        const txt = msg.text();
+        if (txt.includes('intercepted-params:')) {
+            const params = JSON.parse(txt.replace('intercepted-params:', ''));
+            console.log(params);
+
+            try {
+                console.log('Solving the captcha...');
+                const res = await solver.cloudflareTurnstile(params);
+                console.log(`Solved the captcha ${res.id}`);
+                console.log(res);
+                await page.evaluate((token) => {
+                    cfCallback(token);
+                }, res.data);
+            } catch (e) {
+                console.error(e.message);
+                process.exit(1);
+            }
+        }
+    });
+
+    page.goto('https://photon-sol.tinyastro.io/');
+    setTimeout(15000);
+    await page.screenshot({ path: 'screenshot.png' });
+    page_content = await page.content();
+    console.log(page_content);
 })();
