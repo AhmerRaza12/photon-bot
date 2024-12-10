270009a648c2131fb375e3f6ab530446ad5ac356
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-12-10 06:46:45 +0500

    update

--- captcha-solve.js
+++ captcha-solve.js
@@ -19,8 +19,7 @@ puppeteer.use(puppeteerExtraStealth());
     await page.setViewport({ width: 1280, height: 800 });
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
     await page.goto('https://photon-sol.tinyastro.io/');
-    await setTimeout(10000);
-    const page_content = await page.content();
-    console.log(page_content);
+    await page.waitForNetworkIdle();
+    await page.screenshot({ path: 'screenshot.png' });
 
 })();
