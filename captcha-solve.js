497f26c68bcab161631cd077d04239c8477885f4
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-12-10 06:22:56 +0500

    update

--- captcha-solve.js
+++ captcha-solve.js
@@ -1,34 +1,26 @@
+const puppeteerExtraStealth= require('puppeteer-extra-plugin-stealth');
+const puppeteer = require('puppeteer-extra');
+const { setTimeout } = require('timers/promises')
 
-const { connect } = require("puppeteer-real-browser")
+puppeteer.use(puppeteerExtraStealth());
 
-async function test() {
-
-    const { browser, page } = await connect({
-
+(async () => {
+    const browser = await puppeteer.launch({
         headless: false,
-
         args: [
             '--no-sandbox',
-            '--start-maximized',
-            '--auto-open-devtools-for-tabs',
-        ],
+             '--disable-setuid-sandbox',
+             '--start-maximized',
+             '--disable-infobars',
 
-        customConfig: {},
-
-        turnstile: true,
-
-        connectOption: {},
-
-        disableXvfb: false,
-        ignoreAllFlags: false,
-        defaultViewport: null,
-
-    })
-    await page.goto('https://photon-sol.tinyastro.io/')
-
+        ]
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
+    await page.goto('https://photon-sol.tinyastro.io/');
+    await setTimeout(10000);
     const page_content = await page.content();
-    console.log(page_content)
+    console.log(page_content);
 
-}
-
-test()
+})();
